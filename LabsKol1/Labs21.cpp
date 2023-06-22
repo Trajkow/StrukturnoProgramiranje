5cc49e7d337c603f73f81f2ecf51d2c97c36f86e
Trajkow <gligorcetrajkov@gmail.com>
2023-06-22 15:49:53 +0200

    avsd

--- LabsKol1/Labs21.cpp
+++ LabsKol1/Labs21.cpp
@@ -6,33 +6,39 @@ int main() {
     scanf("%d%d", &a, &b);
 
     int counter = 0;
-    int maxCounter = 0;
-    int minCounter = 0;
-    int flag = 0;
-    int devider = 1;
-    int min = 0;
     int max = 0;
+    int maxCounter = 0;
+    int min = 0;
+    int minCounter = 100000;
 
     for (int i = a; i < b; ++i) {
-
-        while (devider <= i) {
-            if (a % devider == 0) {
+        for (int j = 1; j <= i; ++j) {
+            if (i % j == 0) {
                 counter++;
             }
-            devider++;
         }
+        if (counter <= minCounter) {
+            minCounter = counter;
+            min = i;
+        }
+        counter = 0;
+    }
 
-        if (counter > maxCounter) {
+    for (int i = a; i < b; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            if (i % j == 0) {
+                counter++;
+            }
+        }
+        if (counter >= maxCounter) {
             maxCounter = counter;
             max = i;
         }
-
-
+        counter = 0;
     }
 
     printf("Min: %d\n", min);
     printf("Max: %d", max);
-
 
     return 0;
 }
--- LabsKol1/Labs7.cpp
+++ LabsKol1/Labs7.cpp
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-    int p; //broj na postojki
-    int m; //
+    int km;
 
+    int p, m, c;
+    float k;
+
+    int d, z, r;
+    float v;
+
+    scanf("%d", &km);
+    scanf("%d%d%f%d", &p, &m, &k, &c);
+    scanf("%d%d%f%d", &d, &z, &v, &r);
+
+    float firstLine = (float) p * m + (p - 1) * c + (float) km / k;
+    float secondLine = (float) d * z + (d - 1) * r + (float) km / v;
+
+    printf("%d ", (int)firstLine);
+    printf("%d\n", (int)secondLine);
+
+    if (firstLine < secondLine) {
+        printf("1");
+    } else {
+        printf("0");
+    }
 
     return 0;
 }
