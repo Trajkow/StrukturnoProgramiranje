66d512ec08937a9d245df03b97d5d2ea828fe30e
Trajkow <gligorcetrajkov@gmail.com>
2023-07-02 00:57:03 +0200

    Solved

--- KolZadaci/Zadaca13.c
+++ KolZadaci/Zadaca13.c
@@ -5,25 +5,37 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int group1[5], group2[5], gropup3[5];
+    int gr1[100], gr2[100], gr3[100];
+    int counter1 = 0, counter2 = 0, counter3 = 0;
 
     int index;
     for (int i = 0; i < n; ++i) {
         scanf("%d", &index);
 
-        int lastDigit = index % 10;
-
-        if (lastDigit >= 0 && lastDigit <= 2) {
-            group1[i] = index;
-        } else if (lastDigit >= 3 && lastDigit <=5){
-            group2[i] = index;
-        }else if(lastDigit >= 6 && lastDigit <= 9){
-            gropup3[i] = index;
+        if (index % 10 == 0 || index % 10 == 1 || index % 10 == 2) {
+            gr1[counter1] = index;
+            counter1++;
+        } else if (index % 10 == 3 || index % 10 == 4 || index % 10 == 5) {
+            gr2[counter2] = index;
+            counter2++;
+        } else if (index % 10 == 6 || index % 10 == 7 || index % 10 == 8 || index % 10 == 9) {
+            gr3[counter3] = index;
+            counter3++;
         }
     }
 
-    printf("")
-
+    printf("Grupa 1\n");
+    for (int i = 0; i < counter1; ++i) {
+        printf("%d ", gr1[i]);
+    }
+    printf("\nGrupa 2\n");
+    for (int i = 0; i < counter2; ++i) {
+        printf("%d ", gr2[i]);
+    }
+    printf("\nGrupa 3\n");
+    for (int i = 0; i < counter3; ++i) {
+        printf("%d ", gr3[i]);
+    }
 
     return 0;
 }
--- KolZadaci/Zadaca14.c
+++ KolZadaci/Zadaca14.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 
-int main(){
+int maxDigit(int number) {
 
+    int max = 0;
+    while (number) {
+        int digit = number % 10;
+        if (max < digit) {
+            max = digit;
+        }
+        number /= 10;
+    }
+    return max;
+}
 
+int main() {
 
+    int num;
+    int sum = 0;
+    int max = 0;
 
+    while (1) {
 
-
+        if (scanf("%d", &num) != 1) {
+            break;
+        }
+        sum += max;
+        int remember = num;
+        while (num) {
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+        max = maxDigit(remember);
+        printf("%d: %d\n", remember, sum);
+        sum = 0;
+    }
 
     return 0;
 }
