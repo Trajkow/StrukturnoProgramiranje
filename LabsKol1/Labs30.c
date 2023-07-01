3a4582f877268d3ffe685651c206a440c04c7da1
Trajkow <gligorcetrajkov@gmail.com>
2023-07-01 23:51:04 +0200

    Not Solved

--- LabsKol1/Labs30.c
+++ LabsKol1/Labs30.c
@@ -1,19 +1,59 @@
 #include <stdio.h>
 
-int reverseNum(int num) {
-    int reversed = 0;
+int reverseNumber(int number) {
+    int reverse = 0;
+    while (number) {
+        int digit = number % 10;
 
-    while (num) {
-        int digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num /= 10;
+        reverse += digit;
+        number /= 10;
+        reverse *= 10;
     }
-    return reversed;
+    return reverse /= 10;
 }
 
 int main() {
 
+    int n;
+    scanf("%d", &n);
 
+    int sum = 0;
+    int counter = 0;
+    int flag = 0;
+    int num;
+    for (int i = 0; i < n; ++i) {
+
+        scanf("%d", &num);
+
+        int reverse = reverseNumber(num);
+        while (reverse) {
+            int digit = reverse % 10;
+            int secDigit;
+            if (reverse > 9) {
+                secDigit = (reverse % 100) / 10;
+            } else {
+                break;
+            }
+            if (secDigit == 0) {
+                flag = 0;
+                break;
+            }
+            if (digit > secDigit) {
+                flag = 0;
+                break;
+            } else {
+                flag = 1;
+            }
+            reverse /= 10;
+        }
+        if (flag) {
+            sum += num;
+            counter++;
+            printf("%d\n", num);
+        }
+    }
+
+    printf("%d\n%d", sum, counter);
 
     return 0;
 }
