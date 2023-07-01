94a73f48f4d45373023b7fe798f6f75bb176b965
Trajkow <gligorcetrajkov@gmail.com>
2023-07-02 00:14:23 +0200

    Solved

---
+++ LabsKol1/Labs35.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()  {
+    int h, m, s;
+    char forma;
+    scanf("%d %d %d ", &h, &m, &s);
+    scanf("%c", &forma);
+    if(forma == 'A'){
+        if(h == 12){
+            h -= 12;
+        }
+        printf("%02d:%02d:%02d", h, m, s);
+    }
+    else if(forma == 'P'){
+        if(h <= 11){
+            h += 12;
+        }
+        printf("%02d:%02d:%02d", h, m, s);
+    }
+
+    return 0;
+}
+
