1be92714f1745f8aee49906064179a4a878ff87d
Trajkow <gligorcetrajkov@gmail.com>
2023-07-01 16:15:22 +0200

    Solved

--- LabsKol1/Labs18.c
+++ LabsKol1/Labs18.c
@@ -5,9 +5,35 @@ int main() {
     int a, b, c, d, e;
     scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
 
+    int sum = 0;
+    float average = 0;
+    int counter = 0;
+    int whileCounter = 0;
 
+    for (int i = a; i < b; i += c) {
 
+        if ((i * i) % d == 0) {
+            sum += i;
+        }
+        int temp = i;
+        while (temp) {
+            int digit = temp % 10;
+            if (digit == e) {
+                average += i;
+                counter++;
+                break;
+            }
+            whileCounter++;
+            temp /= 10;
+            if(whileCounter == 2){
+                whileCounter = 0;
+                break;
+            }
+        }
+    }
 
+    printf("zbir = %d\n", sum);
+    printf("prosek = %.2f", average / (float) counter);
 
     return 0;
 }
--- LabsKol1/Labs36.c
+++ LabsKol1/Labs36.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
+    int a, b, c, d, e;
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
 
+    int sum = 0;
+    float average = 0.0;
+    int counter = 0;
 
+    for (int i = a; i < b; i += c) {
 
+        if ((i * i) % d == 0) {
+            sum += i;
+        }
+        int temp = i;
+        while (temp) {
+            int digit = temp % 10;
+            if (digit == 1) {
+                average += i;
+                counter++;
+                break;
+            }
+            temp /= 10;
+        }
+    }
 
+    printf("zbir = %d\n", sum);
+    printf("prosek = %.2f", average / counter);
 
     return 0;
 }
