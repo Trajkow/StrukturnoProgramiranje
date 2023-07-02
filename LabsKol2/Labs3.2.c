484bf88f32fef9fc221bc422f160df9a12e1ba34
Trajkow <gligorcetrajkov@gmail.com>
2023-07-02 03:08:07 +0200

    Solved

--- LabsKol2/Labs3.2.c
+++ LabsKol2/Labs3.2.c
@@ -2,6 +2,26 @@
 
 int main() {
 
+    int num;
+    int counter = 0;
+    int arr[100];
+
+    while (1) {
+        if (scanf("%d", &num) != 1) {
+            break;
+        }
+        arr[counter] = num;
+        counter++;
+    }
+
+    for (int i = 0; i < counter; ++i) {
+        for (int j = i+1; j < counter; ++j) {
+            if(arr[i] == arr[j]){
+                arr[j] += (j-i);
+            }
+        }
+        printf("%d ", arr[i]);
+    }
 
 
     return 0;
